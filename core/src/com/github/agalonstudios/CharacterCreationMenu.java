4cd7c4ccff1d4c3c8af377c8637b4dfa272863b9
Jacob <jacobknepper@yahoo.com>
2017-04-03 19:27:24 -0400

    Fixed Slider

--- core/src/com/github/agalonstudios/CharacterCreationMenu.java
+++ core/src/com/github/agalonstudios/CharacterCreationMenu.java
@@ -9,10 +9,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by Jacob on 3/31/2017.
@@ -23,30 +25,23 @@ public class CharacterCreationMenu implements Screen {
     private Stage m_stage;
     private TextButton m_doneButton;
     private Slider m_colorSlider;
+    private Label m_label;
 
     public CharacterCreationMenu(final Agalon a){
 
         m_stage = new Stage();
         Gdx.input.setInputProcessor(m_stage);
+        BitmapFont bfont = new BitmapFont();
 
         Skin skin = new Skin();
-        Skin slideSkin = new Skin();
 
         // creating the button style
         Pixmap pixmap = new Pixmap(100, 100, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.GREEN);
         pixmap.fill();
 
-        // Creating Slider style (setting x to 500 and y to 50 makes the
-        // slider look nicer, but i'm not able to drag it, the button skin works fine)
-        Pixmap pixmapSlider = new Pixmap(500, 50, Pixmap.Format.RGBA8888);
-        pixmapSlider.setColor(Color.GREEN);
-        pixmapSlider.fill();
-
         skin.add("white", new Texture(pixmap));
-        slideSkin.add("white", new Texture(pixmapSlider));
 
-        BitmapFont bfont = new BitmapFont();
         skin.add("default", bfont);
 
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
@@ -59,14 +54,6 @@ public class CharacterCreationMenu implements Screen {
 
         skin.add("default", textButtonStyle);
 
-        Slider.SliderStyle slideStyle = new Slider.SliderStyle();
-        slideStyle.background = slideSkin.newDrawable("white", Color.WHITE);
-        slideStyle.background.setMinWidth(100);
-        slideStyle.knob = slideSkin.newDrawable("white", Color.DARK_GRAY);
-        slideStyle.knobDown = slideSkin.newDrawable("white", Color.PINK);
-        slideStyle.knobOver = slideSkin.newDrawable("white", Color.LIGHT_GRAY);
-
-        slideSkin.add("default", slideStyle);
         // adding the done button
         m_doneButton  = new TextButton("DONE",textButtonStyle);
         m_doneButton.setPosition(250, 250);
@@ -80,10 +67,40 @@ public class CharacterCreationMenu implements Screen {
             }
         });
 
-        // adding the color slider
-        m_colorSlider = new Slider(0, 100, 10, false, slideStyle);
-        m_colorSlider.setPosition(100,50);
+        // add label for slider
+        Label.LabelStyle style = new Label.LabelStyle(bfont, Color.WHITE);
+        m_label = new Label("Choose Character Color", style);
+        m_label.setPosition(250,150);
+        m_stage.addActor(m_label);
+
+        // creating the slider style
+        Pixmap sliderPixmap = new Pixmap(50, 50, Pixmap.Format.RGBA8888);
+        sliderPixmap.setColor(Color.WHITE);
+        sliderPixmap.fill();
+
+        Skin sliderSkin = new Skin();
+        sliderSkin.add("white", new Texture(sliderPixmap));
+
+        Slider.SliderStyle sliderStyle = new Slider.SliderStyle();
+        sliderStyle.knob = skin.newDrawable("white", Color.BLUE);
+        sliderStyle.knobOver = skin.newDrawable("white", Color.BROWN);
+        sliderStyle.knobDown = skin.newDrawable("white", Color.DARK_GRAY);
+        sliderStyle.background = skin.newDrawable("white", Color.WHITE);
+
+        sliderSkin.add("default", sliderStyle);
+        float[] snapvals = new float[]{0,.1f,.2f,.3f,.4f,.5f,.6f,.7f,.8f,.9f,1};
+
+        // adding the slider
+        m_colorSlider  = new Slider(0,1,.1f,false, sliderStyle);
+        m_colorSlider.setWidth(500);
+        // Would like the height to be smaller but can't seem to get the skin
+        // to be smaller with it.
+        m_colorSlider.setHeight(100);
+        m_colorSlider.setPosition(50, 50);
+        m_colorSlider.setSnapToValues(snapvals,.04f);
         m_stage.addActor(m_colorSlider);
+
+        // add preview for character color
 
     }
     @Override
@@ -100,8 +117,8 @@ public class CharacterCreationMenu implements Screen {
         m_stage.setDebugAll(true);
 
         float colorVal;
-        colorVal = m_colorSlider.getPercent();
-
+        colorVal = m_colorSlider.getValue();
+        // use enum for character colors?
 
     }
 
