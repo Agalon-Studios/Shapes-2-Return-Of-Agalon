91d277e80d9a5fb3a2ab10307609be6b10fb8b14
shintoo <seanprapp@gmail.com>
2017-04-06 17:38:43 -0400

    Color square

--- core/src/com/github/agalonstudios/CharacterCreationMenu.java
+++ core/src/com/github/agalonstudios/CharacterCreationMenu.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -21,19 +23,23 @@ import com.badlogic.gdx.utils.Array;
  */
 
 public class CharacterCreationMenu implements Screen {
-
     private Stage m_stage;
     private TextButton m_doneButton;
     private Slider m_colorSlider;
     private Label m_label;
+    private Color m_playerColorHSV;
+    private Color m_playerColorRGB;
 
-    public CharacterCreationMenu(final Agalon a){
+    public CharacterCreationMenu(final Agalon a) {
 
         m_stage = new Stage();
         Gdx.input.setInputProcessor(m_stage);
         BitmapFont bfont = new BitmapFont();
 
         Skin skin = new Skin();
+
+        m_playerColorHSV = new Color(0, 1, .67f, 1);
+        m_playerColorRGB = new Color(224/255.f, 73/255.f, 73/255.f, 1);
 
         // creating the button style
         Pixmap pixmap = new Pixmap(100, 100, Pixmap.Format.RGBA8888);
@@ -88,20 +94,19 @@ public class CharacterCreationMenu implements Screen {
         sliderStyle.background = skin.newDrawable("white", Color.WHITE);
 
         sliderSkin.add("default", sliderStyle);
-        float[] snapvals = new float[]{0,.1f,.2f,.3f,.4f,.5f,.6f,.7f,.8f,.9f,1};
+        //float[] snapvals = new float[]{0,.1f,.2f,.3f,.4f,.5f,.6f,.7f,.8f,.9f,1};
 
         // adding the slider
-        m_colorSlider  = new Slider(0,1,.1f,false, sliderStyle);
+        m_colorSlider  = new Slider(0, 1,.01f, false, sliderStyle);
         m_colorSlider.setWidth(500);
         // Would like the height to be smaller but can't seem to get the skin
         // to be smaller with it.
         m_colorSlider.setHeight(100);
         m_colorSlider.setPosition(50, 50);
-        m_colorSlider.setSnapToValues(snapvals,.04f);
+        //m_colorSlider.setSnapToValues(snapvals,.04f);
         m_stage.addActor(m_colorSlider);
 
         // add preview for character color
-
     }
     @Override
     public void show() {
@@ -116,9 +121,71 @@ public class CharacterCreationMenu implements Screen {
         m_stage.draw();
         m_stage.setDebugAll(true);
 
-        float colorVal;
-        colorVal = m_colorSlider.getValue();
-        // use enum for character colors?
+        m_playerColorHSV.r =  m_colorSlider.getPercent();
+        System.out.println(m_playerColorHSV.r + ", " +  m_colorSlider.getPercent());
+
+        ShapeRenderer sr = ((Agalon) Gdx.app.getApplicationListener()).getShapeRenderer();
+
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        calculateRGB();
+        sr.setColor(m_playerColorRGB.r, m_playerColorRGB.g, m_playerColorRGB.b, 1);
+        sr.rect(Gdx.graphics.getWidth() / 2 - 64, Gdx.graphics.getHeight() / 2 - 64, 64, 64);
+        sr.end();
+    }
+
+    private void calculateRGB() {
+
+        float H = m_playerColorHSV.r;
+        float S = m_playerColorHSV.g;
+        float V = m_playerColorHSV.b;
+
+        System.out.print("(" + H + ", " + S + ", " + V + ") -> ");
+
+
+        float Hdeg = H * 360;
+
+        float Rp, Gp, Bp;
+
+        float C = V * S;
+        float X = C * (1 - Math.abs((Hdeg / 60) % 2 - 1));
+        float M = V - C;
+
+        if (Hdeg >= 0 && Hdeg < 60) {
+            Rp = C;
+            Gp = X;
+            Bp = 0;
+        }
+        else if (Hdeg >= 60 && Hdeg < 120) {
+            Rp = X;
+            Gp = C;
+            Bp = 0;
+        }
+        else if (Hdeg >= 120 && Hdeg < 180) {
+            Rp = 0;
+            Gp = C;
+            Bp = X;
+        }
+        else if (Hdeg >= 180 && Hdeg < 240) {
+            Rp = 0;
+            Gp = X;
+            Bp = C;
+        }
+        else if (Hdeg >= 240 && Hdeg < 300) {
+            Rp = X;
+            Gp = 0;
+            Bp = C;
+        }
+        else {
+            Rp = C;
+            Gp = 0;
+            Bp = X;
+        }
+
+        m_playerColorRGB.r = (Rp + M);
+        m_playerColorRGB.g = (Gp + M);
+        m_playerColorRGB.b = (Bp + M);
+
+        System.out.println("(" + m_playerColorRGB.r + ", " + m_playerColorRGB.g + ", " + m_playerColorRGB.b + ")");
 
     }
 
