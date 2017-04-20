eac837cc0f9dacbf196269a574b948a8c14774a6
Jacob <jacobknepper@yahoo.com>
2017-04-20 18:34:06 -0400

    Dank colors

--- core/src/com/github/agalonstudios/MainMenu.java
+++ core/src/com/github/agalonstudios/MainMenu.java
@@ -101,7 +101,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+        Gdx.gl.glClearColor(135/255f,206/255f, 235/255f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         m_stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         m_stage.draw();
