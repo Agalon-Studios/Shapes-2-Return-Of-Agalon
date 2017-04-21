a3b36f037e6daf1e2fb922949c404d8da12daef6
shintoo <seanprapp@gmail.com>
2017-04-21 01:53:21 -0400

    health bars for all

--- core/src/com/github/agalonstudios/Enemy3.java
+++ core/src/com/github/agalonstudios/Enemy3.java
@@ -1,6 +1,7 @@
 package com.github.agalonstudios;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -68,16 +69,17 @@ public class Enemy3 extends Character {
     @Override
     public void render() {
         super.render();
-
+        ExtendedShapeRenderer er = ((Agalon) Gdx.app.getApplicationListener()).getShapeRenderer();
+        Camera c = ((Agalon)  Gdx.app.getApplicationListener()).getCamera();
         //TODO make in extendedshaperenderer borderedPolygon()
         //  er.setColor(220 / 255.f, 78 / 255.f, 59 / 255.f, 1);
         //  er.rect(m_rect.x + 3 - camera.position.x, m_rect.y + 3 - camera.position.y, m_rect.width - 6, m_rect.height - 6);
 
         //TODO set up hp bars for enemies properly
-//        if (m_engaged) {
-//            er.setColor(255 / 255.f, 0, 0, 1);
-//            er.rect(m_rect.x - camera.position.x, m_rect.y + 10 + m_rect.height - camera.position.y, (m_health / (float) m_maxHealth) * m_rect.width, 5);
-//        }
+        if (m_engaged) {
+            er.setColor(255 / 255.f, 0, 0, 1);
+            er.rect(getX() - c.position.x, getY() + 10 + getHeight() - c.position.y, (m_health / (float) m_maxHealth) * getWidth(), 5);
+        }
     }
 }
 
